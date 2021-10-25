ad1e0fc09fcb93dd6d642a88b1829f5ead36033f
Duarte-Almeida <duartecaladoalmeida@gmail.com>
2021-10-26 00:29:21 +0100

    added more stuff to report and minor fixes

--- hw2/src/ex02.py
+++ hw2/src/ex02.py
@@ -4,6 +4,7 @@ from scipy.io import arff
 from sklearn import feature_selection, model_selection, tree
 import matplotlib.pyplot as plt
 plt.rcParams["text.usetex"] = True
+plt.rcParams["font.serif"] = "Computer Modern Roman"
 
 def load_data(filename):
 
@@ -19,17 +20,19 @@ def compare_accuracy_n_features(inputs, outputs, fold):
 
     test_accuracies = []
     train_accuracies = []
-    for n_features in (1, 3, 5 ,7):
+    for n_features in (1, 3, 5 ,9):
         inputs_new = feature_selection.SelectKBest(feature_selection.mutual_info_classif, k = n_features).fit_transform(inputs, outputs)
+        print(inputs_new)
         results = model_selection.cross_validate(estimator = tree.DecisionTreeClassifier(criterion = "entropy"), X = inputs_new, y = outputs, scoring = "accuracy", cv = fold, return_train_score = True)
         test_accuracies.append(np.mean(results["test_score"]))
         train_accuracies.append(np.mean(results["train_score"]))
     
+    print(f"training accuracies:{train_accuracies} \ntesting accuracies:{test_accuracies}")
     fig, ax = plt.subplots()
     ax.plot([1, 3, 5, 7], test_accuracies, label = "test accuracies")
     ax.plot([1, 3, 5, 7], train_accuracies, label = "train accuracies")
     ax.legend(loc = "best")
-    ax.set_xticks([1,3,5,7])
+    ax.set_xticks([1,3,5,7,9])
 
     plt.savefig("output/accuracy_n_features.pdf")
 
@@ -37,16 +40,16 @@ def compare_accuracy_n_depths(inputs, outputs, fold):
 
     test_accuracies = []
     train_accuracies = []
-    for depth in (1, 3, 5 ,7):
+    for depth in (1, 3, 5 ,9):
         results = model_selection.cross_validate(estimator = tree.DecisionTreeClassifier(criterion = "entropy", max_depth = depth), X = inputs, y = outputs, scoring = "accuracy", cv = fold, return_train_score = True)
         test_accuracies.append(np.mean(results["test_score"]))
         train_accuracies.append(np.mean(results["train_score"]))
-    
+    print(f"training accuracies:{train_accuracies} \ntesting accuracies:{test_accuracies}")
     fig, ax = plt.subplots()
     ax.plot([1, 3, 5, 7], test_accuracies, label = "test accuracies")
     ax.plot([1, 3, 5, 7], train_accuracies, label = "train accuracies")
     ax.legend(loc = "best")
-    ax.set_xticks([1,3,5,7])
+    ax.set_xticks([1,3,5,7,9])
 
     plt.savefig("output/accuracy_n_depths.pdf")
 
