8540533f00b0d32c4e82ee9dcd1a23726af440cf
Duarte-Almeida <duartecaladoalmeida@gmail.com>
2021-10-25 02:05:19 +0100

    minor changes. added latex

---
+++ hw1/src/test.py
@@ -0,0 +1,54 @@
+import numpy as np
+from scipy.io import arff
+from scipy import stats
+from sklearn import model_selection, metrics, neighbors, naive_bayes, feature_selection
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams["text.usetex"] = True
+
+def load_data(filename):
+    dataset = arff.loadarff(filename)
+    dataset = pd.DataFrame(dataset[0])
+    dataset[dataset.columns[-1]] = dataset[dataset.columns[-1]].str.decode('utf8')
+    dataset = dataset.dropna()  #remove instances with missing features
+    return dataset
+
+def plot_grid(dataset):
+    fig, axs = plt.subplots(3, 3)
+    bins = np.linspace(1, 11, 11)           # create bins from 1 to 10
+    for (index, feature) in zip([(i, j) for i in range(0, 3) for j in range(0, 3)], dataset.columns[:-1]):
+        label = feature.replace("_", "\:")
+        _ = axs[index[0], index[1]].hist(dataset[dataset["Class"] == "benign"][feature], bins = bins, density = True, label = f"$p(\mathrm{{{label}|\: Class = benign}})$", alpha = 0.5, rwidth = 0.5, align = "left", color = "green")
+        _ = axs[index[0], index[1]].hist(dataset[dataset["Class"] == "malignant"][feature], bins = bins, density = True, label = f"$p(\mathrm{{{label}|\: Class = malignant}})$", alpha = 0.5, rwidth = 0.5, align = "left", color = "red")
+        axs[index[0], index[1]].legend(loc='best', fontsize = 7.5)
+        axs[index[0], index[1]].set_xticks(np.linspace(1, 10, 10))
+        axs[index[0], index[1]].set_yticks(np.linspace(0, 1, 5))
+        axs[index[0],index[1]].tick_params(axis = 'both', labelsize = 8)
+        axs[index[0], index[1]].set_xlim(right = 11)                # ignore xtick with the number 11
+    fig.set_size_inches(12, 10)
+    plt.savefig(f"output/grid.jpg", dpi = 1200)
+
+def kNN_cross_validation(inputs, outputs):
+    kf = model_selection.KFold(n_splits = 10, shuffle = True, random_state = 76)
+    fig, ax = plt.subplots()
+    test_errors = {}
+    for k in (3, 5, 7):      
+       test_errors[k]=model_selection.cross_validate(estimator=\
+neighbors.KNeighborsClassifier(n_neighbors=k), X = inputs, y = outputs, scoring = "accuracy", cv = kf)["test_score"]
+       print(f"{k}NN accuracy average and variance: {np.mean(test_errors[k])} / {np.var(test_errors[k], ddof = 1)}")
+    bplot = ax.boxplot(test_errors.values())
+    ax.set_xticklabels(["{}-NN".format(k) for k in test_errors.keys()])
+    fig.set_size_inches(6, 2)
+    plt.savefig("output/kNN_performances.png", dpi = 1200)
+
+def test_kNN_NBayes(inputs, outputs):
+    kf = model_selection.KFold(n_splits = 10, shuffle = True, random_state = 76)
+    accuracies_kNN=model_selection.cross_validate(estimator = neighbors.KNeighborsClassifier(n_neighbors = 3), X = inputs, y = outputs, scoring = "accuracy", cv = kf)["test_score"]
+    accuracies_NB = model_selection.cross_validate(estimator = naive_bayes.MultinomialNB(), X = inputs, y = outputs, scoring = "accuracy", cv = kf)["test_score"]
+    result = stats.ttest_rel(accuracies_kNN, accuracies_NB, alternative = "greater")
+    print(f"Statistic:{result.statistic} p-value:{result.pvalue}")
+    
+dataset = load_data("../data/breast.w.arff")
+plot_grid(dataset)
+kNN_cross_validation(inputs=dataset.iloc[:,:-1].values,  outputs=dataset[dataset.columns[-1]].values)
+test_kNN_NBayes(inputs=dataset.iloc[:,:-1].values,  outputs=dataset[dataset.columns[-1]].values)
--- hw2/src/ex01.py
+++ hw2/src/ex01.py
@@ -1,6 +1,15 @@
 import numpy as np
 from termcolor import cprint
 
+def print_matrix(matrix):
+    for i in range(matrix.shape[0]):
+        string = ""
+        for j in range(matrix.shape[1]):
+            string += f" {np.round(matrix[i][j], 4)} &"
+        string = string[:-1] + "\\\\"
+        print(string)
+
+
 # feature mapping
 def phi(x):
     phi = np.zeros(4)
@@ -10,61 +19,63 @@ def phi(x):
 
 def read_data(filename):
     dataset = np.loadtxt(filename, delimiter=",")
-    dataset = np.concatenate((np.ones((dataset.shape[0], 1)), dataset), axis = 1)
     return dataset
 
 def linear_regression(inputs, outputs):
     # Φ
     Phi = np.apply_along_axis(phi, 1, inputs)
     cprint('Φ = ', 'red')
-    print(Phi)
+    print_matrix(Phi)
     
     # Φ^T · Φ
     phi_T_phi = np.matmul(Phi.T, Phi)
     cprint('Φ^T · Φ = ', 'red')
-    print(phi_T_phi)
+    print_matrix(phi_T_phi)
 
     # (Φ^T · Φ)^-1
     inverse = np.linalg.inv(phi_T_phi)
     cprint('(Φ^T · Φ)^-1 = ', 'red')
-    print(inverse)
+    print_matrix(inverse)
 
     # (Φ^T · Φ)^-1 · Φ^T
     pseudo_inverse = np.matmul(inverse, Phi.T)
     cprint('(Φ^T · Φ)^-1 · Φ^T = ', 'red')
-    print(pseudo_inverse)
+    print_matrix(pseudo_inverse)
 
     # W = (Φ^T · Φ)^-1 · Φ^T · z
     weights = np.matmul(pseudo_inverse, outputs)
     cprint('W = (Φ^T · Φ)^-1 · Φ^T · z = ', 'green')
-    print(weights)
+    print_matrix(weights)
 
     return weights
 
 def get_rmse(inputs, outputs, weights):
     predictions = np.matmul(np.apply_along_axis(phi, 1, inputs), weights)
+    cprint('Φ = ', 'green')
+    print_matrix(np.apply_along_axis(phi, 1, inputs))
     cprint('Ẑ = ', 'green')
+    print_matrix(predictions)
     rmse = np.sqrt((np.linalg.norm(predictions - outputs) ** 2) / outputs.shape[0])
     cprint(f'RMSE = {rmse}', 'green', attrs=['bold'])
 
-def categorize_dataset(dataset):
-    median = np.median(dataset[:, [3]])
-    dataset[:, [3]] = np.where(dataset[:, [3]] < median, 0, 1)
-    dataset[:, [4]] = np.where(dataset[:, [4]] < 4, 0, 1)
+#def categorize_dataset(dataset):
+#    median = np.median(dataset[:, [3]])
+#    dataset[:, [3]] = np.where(dataset[:, [3]] < median, 0, 1)
+#    dataset[:, [4]] = np.where(dataset[:, [4]] < 4, 0, 1)
+#
+#    return dataset
 
-    return dataset
-
-def compute_decision_tree(dataset):
-    print("")
+#def compute_decision_tree(dataset):
+#    print("")
     
 def main():
     dataset = read_data("../data/data.csv")
-    train = dataset[:, :8]
-    test = dataset[:, 8:]
+    train = dataset[:8, :]
+    test = dataset[8:, :]
     weights = linear_regression(inputs = train[: , :-1], outputs = train[: , -1:])
     get_rmse(inputs = test[:, :-1], outputs = test[:, -1:], weights = weights)
-    new_dataset = categorize_dataset(dataset)
-    compute_decision_tree(new_dataset)
+    #new_dataset = categorize_dataset(dataset)
+    #compute_decision_tree(new_dataset)
 
 
 if __name__ == "__main__":
