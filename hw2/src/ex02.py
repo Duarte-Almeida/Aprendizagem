f4d8cc1cabe305068bab249d40b8e8ba5b8dd5a3
Duarte-Almeida <duartecaladoalmeida@gmail.com>
2021-10-27 16:35:51 +0100

    check final report

--- hw2/src/ex02.py
+++ hw2/src/ex02.py
@@ -6,65 +6,48 @@ import matplotlib.pyplot as plt
 plt.rcParams["text.usetex"] = True
 
 def load_data(filename):
-
     dataset = arff.loadarff(filename)
     dataset = pd.DataFrame(dataset[0])
-
-    # remove b's in "class" and instances with missing features
     dataset[dataset.columns[-1]] = dataset[dataset.columns[-1]].str.decode('utf8')
     dataset = dataset.dropna()  
     return dataset
 
-def compare_accuracy_n_features(inputs, outputs):
-
+def compare_accuracy(inputs, outputs, folds, mode):
     test_accuracies = []
     train_accuracies = []
-    kf = model_selection.KFold(n_splits = 10, shuffle = True, random_state = 76)
-    for n_features in (1, 3, 5 ,9):
-        inputs_new = feature_selection.SelectKBest(feature_selection.mutual_info_classif, k = n_features).fit_transform(inputs, outputs)
-        results = model_selection.cross_validate(estimator = tree.DecisionTreeClassifier(criterion = "entropy", random_state = 76), X = inputs_new, y = outputs, scoring = "accuracy", cv = kf, return_train_score = True)
+    for param in (1, 3, 5 ,9):
+        if mode == "features":
+            inputs_new = feature_selection.SelectKBest(feature_selection.mutual_info_classif, \
+                                                       k = param).fit_transform(inputs, outputs)
+            estimator = tree.DecisionTreeClassifier(criterion = "entropy", random_state = 76)
+            label = "number\:of\:features"
+        elif mode == "tree_depths":
+            inputs_new = inputs
+            estimator = tree.DecisionTreeClassifier(criterion = "entropy", \
+                                                    max_depth = param, random_state = 76)
+            label = "tree\:depth"
+        results = model_selection.cross_validate(estimator = estimator, \
+                                                 X = inputs_new, y = outputs, \
+                                                 scoring = "accuracy", cv = folds, \
+                                                 return_train_score = True)
         test_accuracies.append(np.mean(results["test_score"]))
         train_accuracies.append(np.mean(results["train_score"]))
-    
-    print(f"training accuracies:{train_accuracies} \ntesting accuracies:{test_accuracies}")
     fig, ax = plt.subplots()
     ax.plot([1, 3, 5, 9], test_accuracies, label = "test accuracies")
     ax.plot([1, 3, 5, 9], train_accuracies, label = "train accuracies")
     ax.set_yticks(test_accuracies + train_accuracies, minor=True)
+    ax.set_xticks([1,3,5,7,9])
     ax.yaxis.grid(True, which='minor', alpha = 0.5, linestyle = "dashed")
     ax.legend(loc = "best")
-    ax.legend(loc = "best")
-    ax.set_xticks([1,3,5,7,9])
+    ax.set_xlabel(label)
+    ax.set_ylabel("accuracy")
+    plt.savefig(f"output/accuracy_n_{mode}.pdf")
 
-    plt.savefig("output/accuracy_n_features.pdf")
-
-def compare_accuracy_n_depths(inputs, outputs):
-
-    test_accuracies = []
-    train_accuracies = []
-    kf = model_selection.KFold(n_splits = 10, shuffle = True, random_state = 76)
-    for depth in (1, 3, 5 ,9):
-        results = model_selection.cross_validate(estimator = tree.DecisionTreeClassifier(criterion = "entropy", max_depth = depth, random_state = 76), X = inputs, y = outputs, scoring = "accuracy", cv = kf, return_train_score = True)
-        test_accuracies.append(np.mean(results["test_score"]))
-        train_accuracies.append(np.mean(results["train_score"]))
-    print(f"training accuracies:{train_accuracies} \ntesting accuracies:{test_accuracies}")
-    fig, ax = plt.subplots()
-    ax.plot([1, 3, 5, 9], test_accuracies, label = "test accuracies")
-    ax.plot([1, 3, 5, 9], train_accuracies, label = "train accuracies")
-    ax.set_yticks(test_accuracies + train_accuracies, minor=True)
-    ax.yaxis.grid(True, which='minor', alpha = 0.5, linestyle = "dashed")
-    ax.legend(loc = "best")
-    ax.set_xticks([1,3,5,7,9])
-
-    plt.savefig("output/accuracy_n_depths.pdf")
-
-def main():
-    dataset = load_data("../data/breast.w.arff")
-    inputs = dataset.iloc[:, :-1].to_numpy()
-    outputs = dataset.iloc[:, [-1]].to_numpy().T.flatten()
-    compare_accuracy_n_features(inputs, outputs)
-    compare_accuracy_n_depths(inputs, outputs)
+dataset = load_data("../data/breast.w.arff")
+inputs = dataset.iloc[:, :-1].to_numpy()
+outputs = dataset.iloc[:, [-1]].to_numpy().T.flatten()
+kf = model_selection.KFold(n_splits = 10, shuffle = True, random_state = 76)
+compare_accuracy(inputs, outputs, kf, "features")
+compare_accuracy(inputs, outputs, kf, "tree_depths")
 
 
-if __name__ == "__main__":
-    main()
