3e90af123002acdaf4a1f3863deeee92c4a84ab6
Duarte-Almeida <duartecaladoalmeida@gmail.com>
2021-10-27 01:37:40 +0100

    all answers given

--- hw2/src/ex02.py
+++ hw2/src/ex02.py
@@ -15,13 +15,14 @@ def load_data(filename):
     dataset = dataset.dropna()  
     return dataset
 
-def compare_accuracy_n_features(inputs, outputs, fold):
+def compare_accuracy_n_features(inputs, outputs):
 
     test_accuracies = []
     train_accuracies = []
+    kf = model_selection.KFold(n_splits = 10, shuffle = True, random_state = 76)
     for n_features in (1, 3, 5 ,9):
         inputs_new = feature_selection.SelectKBest(feature_selection.mutual_info_classif, k = n_features).fit_transform(inputs, outputs)
-        results = model_selection.cross_validate(estimator = tree.DecisionTreeClassifier(criterion = "entropy"), X = inputs_new, y = outputs, scoring = "accuracy", cv = fold, return_train_score = True)
+        results = model_selection.cross_validate(estimator = tree.DecisionTreeClassifier(criterion = "entropy", random_state = 76), X = inputs_new, y = outputs, scoring = "accuracy", cv = kf, return_train_score = True)
         test_accuracies.append(np.mean(results["test_score"]))
         train_accuracies.append(np.mean(results["train_score"]))
     
@@ -37,12 +38,13 @@ def compare_accuracy_n_features(inputs, outputs, fold):
 
     plt.savefig("output/accuracy_n_features.pdf")
 
-def compare_accuracy_n_depths(inputs, outputs, fold):
+def compare_accuracy_n_depths(inputs, outputs):
 
     test_accuracies = []
     train_accuracies = []
+    kf = model_selection.KFold(n_splits = 10, shuffle = True, random_state = 76)
     for depth in (1, 3, 5 ,9):
-        results = model_selection.cross_validate(estimator = tree.DecisionTreeClassifier(criterion = "entropy", max_depth = depth), X = inputs, y = outputs, scoring = "accuracy", cv = fold, return_train_score = True)
+        results = model_selection.cross_validate(estimator = tree.DecisionTreeClassifier(criterion = "entropy", max_depth = depth, random_state = 76), X = inputs, y = outputs, scoring = "accuracy", cv = kf, return_train_score = True)
         test_accuracies.append(np.mean(results["test_score"]))
         train_accuracies.append(np.mean(results["train_score"]))
     print(f"training accuracies:{train_accuracies} \ntesting accuracies:{test_accuracies}")
@@ -60,9 +62,8 @@ def main():
     dataset = load_data("../data/breast.w.arff")
     inputs = dataset.iloc[:, :-1].to_numpy()
     outputs = dataset.iloc[:, [-1]].to_numpy().T.flatten()
-    kf = model_selection.KFold(n_splits = 10, shuffle = True, random_state = 76)
-    compare_accuracy_n_features(inputs, outputs, kf)
-    compare_accuracy_n_depths(inputs, outputs, kf)
+    compare_accuracy_n_features(inputs, outputs)
+    compare_accuracy_n_depths(inputs, outputs)
 
 
 if __name__ == "__main__":
