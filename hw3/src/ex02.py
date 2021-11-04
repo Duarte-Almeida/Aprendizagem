a6bd5d40b83932bec23fc9db443e81ef12738d15
Duarte-Almeida <duartecaladoalmeida@gmail.com>
2021-11-04 14:46:29 +0000

    added some graphs

--- hw3/src/ex02.py
+++ hw3/src/ex02.py
@@ -1,45 +1,53 @@
 import numpy as np
 import pandas as pd
 from scipy.io import arff
-from sklearn import neural_network, model_selection, preprocessing, feature_selection, metrics
+from sklearn import neural_network, model_selection, preprocessing, metrics
 import matplotlib.pyplot as plt
 plt.rcParams["text.usetex"] = True
 
 def load_data(filename):
     dataset = arff.loadarff(filename)
     dataset = pd.DataFrame(dataset[0])
-    # REVIEW: this decode breaks for kin dataset but is necessary for breast_data
-    try:
-        dataset[dataset.columns[-1]] = dataset[dataset.columns[-1]].str.decode('utf8')
-    except AttributeError:
-        # kin will pass this
-        pass
+    str_columns = [col for col in dataset.columns if dataset[col].dtype == "object"]
+    dataset[str_columns] = dataset[str_columns].apply(lambda x: x.str.decode('utf8'))
     dataset = dataset.dropna()  
     return dataset
 
 #  MLP with l2 regularization, RELU activation function, 2 hidden layers of size 3,2 and remaining default parameters.
-def mlp_predict(func ,inputs, outputs, folds, early_stopping, alpha):
-    clf = func(activation = 'relu', solver = 'sgd', \
-                                            hidden_layer_sizes = (3, 2), \
+def mlp_predict(mlp_model ,inputs, outputs, folds, early_stopping, alpha):
+    clf = mlp_model(activation = 'relu',    hidden_layer_sizes = (3, 2), \
                                             random_state = 76, \
                                             early_stopping = early_stopping, \
-                                            alpha=alpha, \
-                                            max_iter=1500)
-    return model_selection.cross_val_predict(clf, inputs, outputs, cv=folds)
+                                            alpha = alpha, \
+                                            max_iter = 1500)
+    return model_selection.cross_val_predict(clf, inputs, outputs, cv = folds)
 
 # Plots the confusion matrix for a given MLP with/without early stopping
 def mlp_conf_matrix(inputs, outputs, folds, early_stopping):
     outputs_pred = mlp_predict(neural_network.MLPClassifier, inputs, outputs, folds, early_stopping, 1)
-    conf_mat = metrics.confusion_matrix(outputs, outputs_pred)
-    disp = metrics.ConfusionMatrixDisplay(confusion_matrix=conf_mat, display_labels=['Benign','Malign']).plot()
-    disp.ax_.set(xlabel='Predicted', ylabel='True', \
-        title=f'Confusion Matrix {"(with early stopping)" if early_stopping else "(without early stopping)"}')
-
+    disp = metrics.ConfusionMatrixDisplay.from_predictions(outputs, outputs_pred)
+    disp.ax_.set(title=f'Confusion Matrix {"(with early stopping)" if early_stopping else "(without early stopping)"}')
+    plt.savefig(f"output/mlp_conf_matrix_{early_stopping}.pdf")
 
 # Returns the residues of the distribution with/without regularization
-def residue_dist_bp(inputs, outputs, folds, regularization):
-    return outputs - mlp_predict(neural_network.MLPRegressor, inputs, outputs, folds, True, 1 if regularization else 1e-5)
+def residue_dist_bp(inputs, outputs, folds):
+    res_regularized = outputs - mlp_predict(neural_network.MLPRegressor, inputs, outputs, folds, True, 4)
+    res_nonregularized = outputs - mlp_predict(neural_network.MLPRegressor, inputs, outputs, folds, True, 0)
+    #np.savez("res.txt", res_regularized, res_nonregularized)
 
+    #npzfile = np.load("res.txt.npz")
+    #res_regularized = npzfile["arr_0"]
+    #res_nonregularized = npzfile["arr_1"]
+
+    fig, ax = plt.subplots()
+    bp = ax.boxplot([res_regularized, res_nonregularized], vert = False, flierprops={'marker': 'o', 'markersize': 1})
+
+    ax.set_yticklabels(['without regularization','with regularization \n ($\\alpha = 4$)'])
+    ax.tick_params(axis = u'y', length = 0)
+    fig.set_size_inches(12, 6)
+    
+    plt.savefig("output/residue_boxplot.pdf")
+   
 
 def main():
     kf = model_selection.KFold(n_splits = 5, shuffle = True, random_state = 0)
@@ -55,17 +63,8 @@ def main():
     kin_data = load_data("../data/kin8nm.arff")
     inputs_kin = kin_data.iloc[:, :-1].to_numpy()
     outputs_kin = kin_data.iloc[:, [-1]].to_numpy().T.flatten()
-    residue_bp1 = residue_dist_bp(inputs_kin, outputs_kin, kf, True)
-    residue_bp2 = residue_dist_bp(inputs_kin, outputs_kin, kf, False)
-    residues = np.column_stack((residue_bp1, residue_bp2))
+    residue_dist_bp(inputs_kin, outputs_kin, kf)
     
-    fig, disp = plt.subplots()
-    disp.set_title('Multiple Samples with Different sizes')
-    disp.boxplot(residues, vert=False)
-    disp.set_axisbelow(True)
-    disp.set_yticklabels(['with regularization','without regularization'])
-    disp.set_title('Comparison of residues in the presence and absence of regularization')
-    plt.show()
 
 if __name__ == "__main__":
     main()
