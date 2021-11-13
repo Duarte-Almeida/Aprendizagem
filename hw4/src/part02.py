16f808828d526ab096669f3df2cf286c33fa8712
Duarte-Almeida <duartecaladoalmeida@gmail.com>
2021-11-13 17:49:08 +0000

    final

--- hw4/src/part02.py
+++ hw4/src/part02.py
@@ -1,10 +1,11 @@
-from joblib.logger import PrintTime
 import numpy as np
 import pandas as pd
 from scipy.io import arff
+from scipy.stats import mode
 from sklearn import cluster, metrics, feature_selection
 import matplotlib.pyplot as plt
-#plt.rcParams["text.usetex"] = True
+from collections import Counter
+plt.rcParams["text.usetex"] = True
 
 def load_data(filename):
     dataset = arff.loadarff(filename)
@@ -14,41 +15,47 @@ def load_data(filename):
     dataset = dataset.dropna()  
     return dataset
 
-def compute_ecr(labels, outputs, k):
-    benign_count = [0] * k
-    malign_count = [0] * k
+# ECR is the mean over all cluster of cluster_size - label_mode_in_each_cluster
+def compute_ecr(labels, outputs):
 
-    for i in range(0, k):
-        for j in range(0, len(labels)):
-            if labels[j] == i:
-                if outputs[j] == "benign":
-                    benign_count[i]+=1
-                else:
-                    malign_count[i]+=1
+    cluster_indexes = np.unique(labels)
+    ecr_measure = 0
     
-    aux = np.absolute(np.array(benign_count) - np.array(malign_count))
-    return (1/k) * (np.sum(aux))
-
-### MAIN ### 
+    for index in cluster_indexes:
+        cluster = outputs[labels == index]  # get instances in each cluster
+        c = cluster.shape[0]
+        phi = np.amax(np.unique(cluster, return_counts = True)[1]) # get count of majority label in cluster
+        ecr_measure += c - phi
+    
+    return ecr_measure / cluster_indexes.shape[0]
+                             
 data = load_data("../data/breast.w.arff")
 inputs = data.iloc[:, :-1].to_numpy()
 outputs = data.iloc[:, [-1]].to_numpy().T.flatten()
-kmeans_2 = cluster.KMeans(n_clusters = 2, random_state = 76).fit(inputs)
-kmeans_3 = cluster.KMeans(n_clusters = 3, random_state = 76).fit(inputs)
+labels_2means = cluster.KMeans(n_clusters = 2, random_state = 76).fit_predict(inputs)
+labels_3means = cluster.KMeans(n_clusters = 3, random_state = 76).fit_predict(inputs)
 
-# 4) a.
-print(f"ECR k = 2: {compute_ecr(kmeans_2.labels_, outputs, 2)}")
-print(f"ECR k = 3: {compute_ecr(kmeans_3.labels_, outputs, 3)}")
+print(f"ECR k = 2: {compute_ecr(labels_2means, outputs)}")
+print(f"ECR k = 3: {compute_ecr(labels_3means, outputs)}")
 print(" --- ")
 
-# 4) b.
-print(f"Silhouette score k = 2: {round(metrics.silhouette_score(inputs, kmeans_2.labels_), 5)}")
-print(f"Silhouette score k = 3: {round(metrics.silhouette_score(inputs, kmeans_3.labels_), 5)}")
+print(f"Silhouette score k = 2: {round(metrics.silhouette_score(inputs, labels_2means), 5)}")
+print(f"Silhouette score k = 3: {round(metrics.silhouette_score(inputs, labels_3means), 5)}")
 
-# 5)
-kBest = feature_selection.SelectKBest(feature_selection.mutual_info_classif, k = 2).fit(inputs, outputs)
-inputs_top_features = inputs[:, kBest.get_support(indices=True)]
-kmeans_3 = cluster.KMeans(n_clusters=3, random_state=76).fit(inputs_top_features)
-plt.scatter(inputs_top_features[:,0], inputs_top_features[:,1], c=kmeans_3.labels_, cmap='rainbow')
-plt.scatter(kmeans_3.cluster_centers_[:,0] ,kmeans_3.cluster_centers_[:,1], color='k')
-plt.show()
+mutual_info_3best = feature_selection.SelectKBest(feature_selection.mutual_info_classif, k = 2)
+inputs_top_features = mutual_info_3best.fit_transform(inputs, outputs)
+extracted_features = data.iloc[:, :-1].columns[mutual_info_3best.get_support()].values
+clusters_3means = cluster.KMeans(n_clusters = 3, random_state = 76).fit(inputs_top_features)
+
+fig, ax = plt.subplots()
+for index, c in zip(np.unique(clusters_3means.labels_), ("indianred", "gold", "darkslategrey")): #TODO: select another color palette
+    #weights = [2*i for i in Counter(inputs_top_features[:, 0][clusters_3means.labels_ == index]).values() for j in range(i)]
+    ax.scatter(inputs_top_features[:, 0][clusters_3means.labels_ == index], \
+                inputs_top_features[:, 1][clusters_3means.labels_ == index], color = c)
+
+#plt.scatter(inputs_top_features[:, 0], inputs_top_features[:, 1], c = clusters_3means.labels_, cmap='cividis') #TODO: check this
+
+ax.scatter(clusters_3means.cluster_centers_[:, 0] , clusters_3means.cluster_centers_[:, 1], color = "k", marker = "+", linewidths = 0.5, s = 150) 
+ax.set_xlabel(extracted_features[0].replace("_", "\;"))
+ax.set_ylabel(extracted_features[1].replace("_", "\;"))
+plt.savefig("output/3means.pdf")
